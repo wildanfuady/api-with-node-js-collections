5fbe17345c5ce10c8d1558dba16ed0eb6448142f
wildanfuady <wildanfuady@gmail.com>
2021-05-16 13:02:42 +0700

    :rocket:

---
+++ node_auth_passport/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
+    },
+	'database': 'db_node_password_mysql',
+    'users_table': 'users'
+};
